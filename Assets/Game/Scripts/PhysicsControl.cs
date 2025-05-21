fd7adece5ca224094c128b2e6ebc3ca104a1ccc8
pojitonov <pojitonov@gmail.com>
2025-05-21 15:51:03 +0300

    added states for physics controlls

--- Assets/Game/Scripts/PhysicsControl.cs
+++ Assets/Game/Scripts/PhysicsControl.cs
@@ -1,12 +1,48 @@
+using System.Collections.Generic;
+using Oculus.Interaction;
 using UnityEngine;
 
 public class PhysicsControl : MonoBehaviour
 {
-    [SerializeField] private Transform rootObject;
-    
+    [SerializeField] private Transform rootObjectPhysics;
+    [SerializeField] private Transform physicsControls;
+    [SerializeField] private List<Transform> defaultButtons;
+
+    private void Start()
+    {
+        foreach (Transform child in physicsControls.GetComponentsInChildren<Transform>(true))
+        {
+            if (child.name == "ButtonPanelBack")
+            {
+                var rbp = child.GetComponent<RoundedBoxProperties>();
+                if (rbp != null)
+                {
+                    rbp.Color = new Color32(0, 0, 0, 51);
+                    rbp.SendMessage("UpdateMaterialPropertyBlock", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+        
+        foreach (var button in defaultButtons)
+        {
+            foreach (Transform child in button.GetComponentsInChildren<Transform>(true))
+            {
+                if (child.name == "ButtonPanelBack")
+                {
+                    var rbp = child.GetComponent<RoundedBoxProperties>();
+                    if (rbp != null)
+                    {
+                        rbp.Color = new Color(0f, 1f, 0f, 0.5f);
+                        rbp.SendMessage("UpdateMaterialPropertyBlock", SendMessageOptions.DontRequireReceiver);
+                    }
+                }
+            }
+        }
+    }
+
     public void EnablePhysics()
     {
-        foreach (Transform child in rootObject)
+        foreach (Transform child in rootObjectPhysics)
         {
             if (child.TryGetComponent<Rigidbody>(out var rb))
             {
@@ -14,10 +50,10 @@ public class PhysicsControl : MonoBehaviour
             }
         }
     }
-    
+
     public void DisablePhysics()
     {
-        foreach (Transform child in rootObject)
+        foreach (Transform child in rootObjectPhysics)
         {
             if (child.TryGetComponent<Rigidbody>(out var rb))
             {
@@ -25,10 +61,10 @@ public class PhysicsControl : MonoBehaviour
             }
         }
     }
-    
+
     public void ChangeDamping(float value)
     {
-        foreach (Transform child in rootObject)
+        foreach (Transform child in rootObjectPhysics)
         {
             if (child.TryGetComponent<Rigidbody>(out var rb))
             {
@@ -36,14 +72,46 @@ public class PhysicsControl : MonoBehaviour
             }
         }
     }
-    
+
     public void JointAnchorY(float value)
     {
-        foreach (Transform child in rootObject)
+        foreach (Transform child in rootObjectPhysics)
         {
             if (child.TryGetComponent<HingeJoint>(out var joint))
             {
                 joint.anchor = new Vector3(0f, value, 0f);
+            }
+        }
+    }
+
+    public void SetActiveState(Transform buttonRoot)
+    {
+        foreach (Transform child in buttonRoot.GetComponentsInChildren<Transform>(true))
+        {
+            if (child.name == "ButtonPanelBack")
+            {
+                var rbp = child.GetComponent<RoundedBoxProperties>();
+                if (rbp != null)
+                {
+                    rbp.Color = new Color(0f, 1f, 0f, 0.5f);
+                    rbp.SendMessage("UpdateMaterialPropertyBlock", SendMessageOptions.DontRequireReceiver);
+                }
+            }
+        }
+    }
+
+    public void SetInactiveState(Transform buttonRoot)
+    {
+        foreach (Transform child in buttonRoot.GetComponentsInChildren<Transform>(true))
+        {
+            if (child.name == "ButtonPanelBack")
+            {
+                var rbp = child.GetComponent<RoundedBoxProperties>();
+                if (rbp != null)
+                {
+                    rbp.Color = new Color32(0, 0, 0, 51);
+                    rbp.SendMessage("UpdateMaterialPropertyBlock", SendMessageOptions.DontRequireReceiver);
+                }
             }
         }
     }
