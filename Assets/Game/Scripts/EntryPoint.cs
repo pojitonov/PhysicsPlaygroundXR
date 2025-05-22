24a97b97d240e1b471e1a50d96c0ca6b4b8e7007
pojitonov <pojitonov@gmail.com>
2025-05-22 11:47:07 +0300

    fixed occlusion mesh for recordings

---
+++ Assets/Game/Scripts/EntryPoint.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.XR;
+
+namespace Game.Scripts
+{
+    public class EntryPoint : MonoBehaviour
+    {
+        private void Start()
+        {
+            XRSettings.useOcclusionMesh = false;
+        }
+    }
+}
